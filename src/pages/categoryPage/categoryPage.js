2d372f2671931db344d8e2d76632c5132414e830
Adimula3 <Kolawoleolayinka16@gmail.com>
2022-09-12 11:14:54 -0500

    commerce

--- src/pages/categoryPage/categoryPage.js
+++ src/pages/categoryPage/categoryPage.js
@@ -1,24 +1,22 @@
 import React, {useEffect, useState} from "react";
 import data from '../../data/data.json'
 
-const CategoryPage = () => {
+const CategoryPage = ({match}) => {
 
     const [categoryItems, setCategoryItems] = useState([]);
 
-    const testFetch = async () => {
-        try{
-            console.log(data);
-        }
-        catch (error) {
-            console.log(error)
-        }
-    }
-
     useEffect(() => {
-       if(isMounted) {
-           testFetch().then()
-       }
-    }, []);
+        getCategory();
+        window.scrollTo(0,0);
+    }, [match.params.category])
+
+    const getCategory = () => {
+
+                const categoryList = data.filter(item => item.category === match.params.category).reverse();
+                setCategoryItems(categoryList)
+
+
+    }
 
 
 
